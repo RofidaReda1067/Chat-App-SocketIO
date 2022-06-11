cdcc2ae54b74282dfb0842255c680f0bac2db091
RofidaReda <57877648+ROFIDA106@users.noreply.github.com>
2022-06-11 04:57:55 +0200

    editServer

--- server.js
+++ server.js
@@ -24,33 +24,20 @@ const io = new Server(server, {
 });
 /////////////////////////////////////////////////
 //Connection
+const users = [];
 io.on("connection", (socket) => {
+  console.log(socket.handshake.query.name);
   console.log("lab2 pushAndPull");
   //Room Creation
-  socket.on("join", ({ NAME, ROOM }, callback) => {
-    const { error, user } = addUser({ ID: socket.id, NAME, ROOM });
 
-    if (error) return callback(error);
-    socket.join(user.ROOM);
-    //To send Messages
-    socket.emit("message", {
-      user: "Admin",
-      text: `${user.NAME},Welcome to Chat Room ${user.ROOM}.`,
-    });
-    socket.broadcast
-      .to(user.ROOM)
-      .emit("message", { user: "Admin", text: `${user.NAME} Has Joined!` });
-    io.to(user.ROOM).emit("ROOMData", {
-      ROOM: user.ROOM,
-      users: getUsersInRoom(user.ROOM),
-    });
+  const user = { ID: socket.id, NAME: socket.handshake.query.name };
+  users.push(user);
+  console.log(users);
 
-    callback();
-  });
   //To Recieve Messages
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
-    io.to(user.ROOM).emit("message", { user: user.NAME, text: message });
+    // io.to(user.ROOM).emit("message", { user: user.NAME, text: message });
 
     callback();
   });
@@ -59,36 +46,31 @@ io.on("connection", (socket) => {
   socket.on("diconnection", () => {
     const user = removeUser(socket.id);
 
-    if (user) {
-      io.to(user.ROOM).emit("message", {
-        user: "Admin",
-        text: `${user.NAME} Has Left.`,
-      });
-      io.to(user.ROOM).emit("ROOMData", {
-        room: user.ROOM,
-        users: getUsersInRoom(user.ROOM),
-      });
-    }
+    // if (user) {
+    //   io.to(user.ROOM).emit("message", {
+    //     user: "Admin",
+    //     text: `${user.NAME} Has Left.`,
+    //   });
+    //   io.to(user.ROOM).emit("ROOMData", {
+    //     room: user.ROOM,
+    //     users: getUsersInRoom(user.ROOM),
+    //   });
+    //}
   });
 });
 ///////////////////////////////////////////
-const users = [];
 //ADD uSER
-const addUser = ({ ID, NAME, ROOM }) => {
-  NAME = NAME.trim().toLowerCase();
-  ROOM = ROOM.trim().toLowerCase();
-  const currUser = users.find(
-    (users) => users.ROOM === ROOM && users.NAME === Name
-  );
+// const addUser = ({ ID, NAME }) => {
+// NAME = NAME.trim().toLowerCase();
+// ROOM = ROOM.trim().toLowerCase();
+// const currUser = users.find((users) => users.NAME === Name);
 
-  //validation
-  if (currUser) {
-    return { error: "User is Already here!" };
-  }
-  const user = { ID, NAME, ROOM };
-  users.push(user);
-  return { user };
-};
+//validation
+// if (currUser) {
+//   return { error: "User is Already here!" };
+// }
+///////////////////
+
 ////////////////////////////////////////////
 // REMOVE USER
 const removeUser = (ID) => {
@@ -101,7 +83,7 @@ const removeUser = (ID) => {
 //GET USER
 const getUser = (ID) => users.find((user) => user.ID === ID);
 //GET USERS IN ROOM
-const getUsersInRoom = (room) => users.filter((user) => user.ROOM === ROOM);
+// const getUsersInRoom = (room) => users.filter((user) => user.ROOM === ROOM);
 //test validation
 const Valid = (req, res) => {
   res.send("Server is running now !!");
